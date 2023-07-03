5c3977635fa435f05bfbc8f0f445d5883428631e
Rob Scherer <robscherer6@gmail.com>
2023-07-03 07:29:32 -0400

    basic styling added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
+import Header from './components/Header'
+
 function App() {
-  const title = 'Blog Post'
-  const body = 'this is my blog post'
-  const comments = [
-    {id: 1, text: 'Comment one'},
-    {id: 2, text: 'Comment two'},
-    {id: 3, text: 'Comment seven thousand'}
-  ]
-
-  const loading = false
-  const showComments = true
-
-  if (loading) return <h1>Loading...</h1>
 
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && (<div className="comments">
-        <h3>Comments ({comments.length}):</h3>
-        <ul>
-          {comments.map((comment) => (
-            <li key={comment.id}>{comment.text}</li>
-          ))}
-        </ul>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
       </div>
-      )}
-    </div>
+    </>
   )
 }
 
