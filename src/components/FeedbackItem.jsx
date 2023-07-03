9f406a7d4f18dbbcfa7f4833fdc634bc6f375221
Rob Scherer <robscherer6@gmail.com>
2023-07-03 15:58:58 -0400

    dynamically added feedback items using app-level (global) state

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,21 +1,8 @@
-import {useState} from 'react'
-
-function FeedbackItem() {
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('Do your homework!!')
-
-  const handleClick = () => {
-    setRating(10)
-    // setRating((prev) => {
-    //   return prev + 1;
-    // });
-  }
-
+function FeedbackItem({item}) {
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   )
 }
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Do you homework or be ill-prepared for the exam.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Bacon does make everything better but it comes with a health consequence.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'If I had a dollar for every time someone said I was the smartest person in the room I would have zero dollars.',
+  },
+]
+
+export default FeedbackData
