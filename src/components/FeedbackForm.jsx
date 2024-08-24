7ae5a52e8d5d6e8dc48a3f73f07d4ded5239eb9c
Rob Scherer <robscherer6@gmail.com>
2024-08-24 08:06:45 -0400

    state not updating as expected

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import Card from "./shared/Card";
-import Button from "./shared/Button"
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 const FeedbackForm = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
-    if (text === '') {
-        setBtnDisabled(true);
-        setMessage(null);
-    } else if (text.length !== '' && text.trim().length <= 10) {
-        setBtnDisabled(true);
-        setMessage('Text must be at least 10 characters.');
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text.length !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters.");
     } else {
-        setMessage(null);
-        setBtnDisabled(false);
+      setMessage(null);
+      setBtnDisabled(false);
     }
     setText(e.target.value);
   };
@@ -24,7 +26,7 @@ const FeedbackForm = () => {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating) => console.log(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -32,9 +34,11 @@ const FeedbackForm = () => {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit" version='secondary' isDisabled={btnDisabled}>Send</Button>
+          <Button type="submit" version="secondary" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
-        {message && <div className='message'>{message}</div>}
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
